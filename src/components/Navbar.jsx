e32bcf660fc5a6cc4f13435a8b8ef5c7f9ac1c3b
imed0691 <imedexas@email.com>
2025-05-17 21:44:08 +0200

    Fix: affichage du logo importé dans Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useCart } from '../context/useCart';
+import logoImg from '../assets/logo.jpg';
 
 const Navbar = () => {
   const { cart } = useCart();
@@ -7,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        <img src="public\images\logo.jpg" alt="Logo" width="50" />
+        <img src={logoImg} alt="Logo" width="50" />
       </Link>
       
       <div className="nav-links">
